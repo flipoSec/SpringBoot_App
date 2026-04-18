6a13af72242be09fbde4766c246c77a2433ba74f
flipoSec <220247641+flipoSec@users.noreply.github.com>
2026-04-18 15:04:43 +0100

    fix api url

--- React/React/src/api/axios.js
+++ React/React/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "https://localhost:7999/api",
+    baseURL: "http://localhost:9090/api",
 });
 
 api.interceptors.request.use((config) => {
